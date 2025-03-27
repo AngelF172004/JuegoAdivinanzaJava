642a6b48282b4017164c76d2178f1eb08b5678f1
Daniel Cortés <Danycorts1@gmail.com>
2025-03-27 16:51:17 -0600

    Actualizar GeneradorNumero.java

--- GeneradorNumero.java
+++ GeneradorNumero.java
@@ -1,10 +1,7 @@
-import java.util.Random;
-
-public class GeneradorNumero {
-    public static int aleatorio() { // Método para generar un número aleatorio
+    public static int aleatorio(){ //Se genera el numero random
         Random random = new Random();
         int min = 1;
         int max = 100;
-        return random.nextInt(max - min + 1) + min;
+        int randomNumber = random.nextInt(max - min + 1) + min;
+        return randomNumber;
     }
-}
