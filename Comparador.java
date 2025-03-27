2c3e83d52c33746a9a08bd19902c7ed0e21f2d62
Daniel Cortés <Danycorts1@gmail.com>
2025-03-27 16:49:47 -0600

    Actualizar Pistas.java

--- Comparador.java
+++ Comparador.java
@@ -1,13 +1,14 @@
-public class Comparador {
-    public static boolean pistas(int opcion, int numeroaleatorio) { // Compara los números
+    public static boolean pistas(int opcion, int numeroaleatorio){ //El metodo que compara los numeros
         if (opcion == numeroaleatorio) {
             System.out.println("\n\n\t\t\t\tADIVINASTE!!!\n\n\t\t\t\tFELICIDADES!!!");
             return true;
-        } else if (opcion > numeroaleatorio) {
-            System.out.println("El número a adivinar es menor :(");
-        } else {
-            System.out.println("El número a adivinar es mayor :(");
         }
-        return false;
+        else if (opcion > numeroaleatorio) {
+            System.out.println("El numero a adivinar es menor :(");
+            return false;
+        }
+        else {
+            System.out.println("El numero a adivinar es mayor :(");
+            return false;
+        }
     }
-}
